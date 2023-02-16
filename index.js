d37137a35eefa537319a8941fde45386a039271b
Gaurav Kukreti <gauravkukreti02@gmail.com>
2023-02-17 00:22:07 +0530

    updated js

---
+++ index.js
@@ -0,0 +1,16 @@
+function backspace(x)
+{
+    var size = x.length;
+    var x1=x.substring(0,size-1);
+    return x1;
+}
+
+function power1(x)
+{
+    return 0;
+}
+
+function factorial(x)
+{
+    return 1;
+}
